6b55c6323ea3eb619b8a3600b88423c801ca3330
sterenziani <sterenziani@itba.edu.ar>
2020-10-12 14:40:29 -0300

    Added normalization step to bonus exercise

--- src/EjBonus/main.py
+++ src/EjBonus/main.py
@@ -5,12 +5,29 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
 
-CSV_PATH = 'files/europe_positive.csv'
+CSV_PATH = 'files/europe.csv'
 
 my_csv = CSV_PATH
 df = pd.read_csv(my_csv)
 features = ['Area','GDP','Inflation','Life.expect','Military','Pop.growth','Unemployment']
 X = df[features]
+
+# Normalizar
+X[['Area']] -= min(X[['Area'][0]])
+X[['Area']] /= max(X[['Area'][0]])
+X[['GDP']] -= min(X[['GDP'][0]])
+X[['GDP']] /= max(X[['GDP'][0]])
+X[['Inflation']] -= min(X[['Inflation'][0]])
+X[['Inflation']] /= max(X[['Inflation'][0]])
+X[['Life.expect']] -= min(X[['Life.expect'][0]])
+X[['Life.expect']] /= max(X[['Life.expect'][0]])
+X[['Military']] -= min(X[['Military'][0]])
+X[['Military']] /= max(X[['Military'][0]])
+X[['Pop.growth']] -= min(X[['Pop.growth'][0]])
+X[['Pop.growth']] /= max(X[['Pop.growth'][0]])
+X[['Unemployment']] -= min(X[['Unemployment'][0]])
+X[['Unemployment']] /= max(X[['Unemployment'][0]])
+# End of normalization
 
 pca = PCA()
 components = pca.fit_transform(X)
