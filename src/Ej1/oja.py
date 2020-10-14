2d818094cee255135678b6e42619806432efe1a6
sterenziani <sterenziani@itba.edu.ar>
2020-10-14 18:33:31 -0300

    Added graphs and error comparison for Exercise 1b

--- src/Ej1/oja.py
+++ src/Ej1/oja.py
@@ -2,7 +2,9 @@ import random
 import math
 import matplotlib.colors as colors
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 import numpy as np
+import pandas as pd
 import statistics
 
 class Oja:
@@ -15,7 +17,7 @@ class Oja:
         self.w = [0 for w in range(self.input_neurons)]
         for i in range(len(self.w)):
             self.w[i] = random.uniform(-0.5, 0.5)
-        self.epochs = 10000
+        self.epochs = 5000
 
     def pre_process(self, grid):
         normalized_columns = []
@@ -35,9 +37,29 @@ class Oja:
             y += self.w[j] * input[j]
         return y
 
-    def train(self):
+    def pca(self, path):
+        data = pd.read_csv(path)
+        features = ['Area','GDP','Inflation','Life.expect','Military','Pop.growth','Unemployment']
+        X_standarized = data[features]
+        X_standarized = pd.DataFrame(data=self.pre_process(X_standarized.to_numpy()))
+        correlations_matrix = X_standarized.corr()
+        eigen_values_s, eigen_vectors_s = np.linalg.eig(correlations_matrix)
+        idx_order_s = eigen_values_s.argsort()[::-1]
+        eigen_values_s = eigen_values_s[idx_order_s]
+        eigen_vectors_s = eigen_vectors_s[:,idx_order_s]
+        pca_s = PCA()
+        main_components_s = pca_s.fit_transform(X_standarized)
+        loadings_s = pca_s.components_.T
+        loadings_matrix_s = pca_s.components_.T * np.sqrt(pca_s.explained_variance_)
+        names = list(data['Country'])
+        y1_s = pca_s.fit_transform(X_standarized)[:,0]
+        return loadings_s.transpose()[0], y1_s
+
+    def train(self, path):
+        real_w, real_scores = self.pca(path)
         data = self.data
         t = 1
+        weight_error = np.zeros((self.epochs, len(self.w)))
         for n in range(self.epochs):
             random.shuffle(data)
             for x in data:
@@ -46,8 +68,27 @@ class Oja:
                 for j in range(N):
                     self.w[j] += (self.learning_rate/t) * (y * x[j] - y * y * self.w[j])
                 t += 1
+            weight_error[n] = abs(self.w - real_w)
+        plt.plot(range(self.epochs), weight_error)
+        plt.xlabel("Epochs")
+        plt.ylabel("Error")
+        plt.title('Weight error')
+        plt.show()
 
-    def test(self, names, data):
+        # Si los signos quedaron dados vuelta, invertirlos. El error ya fue calculado así que fue
+        if self.w[0] * real_w[0] < 0:
+            self.w = [-self.w[i] for i in range(len(self.w))]
+        print("Final weight error:")
+        print(weight_error[-1])
+
+    def test(self, names, data, path):
+        real_w, real_scores = self.pca(path)
         data = self.pre_process(data)
+        i = 0
+        print("\n{0:<10}\t{1:<20}\t{2:<20}\t{3:<20}".format("COUNTRY","SCORE","EXPECTED","ERROR"))
         for name, x in zip(names, data):
-            print(name, self.calculate(x))
+            score = self.calculate(x)
+            real_score = real_scores[i]
+            error = abs(score - real_score)
+            print("{0:<10}\t{1:<10}\t{2:<10}\t{3:<10}".format(name, score, real_score, error))
+            i += 1
